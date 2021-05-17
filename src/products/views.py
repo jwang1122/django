ad1f4cb750e43d3e7c7e08247d1307aa3dea3967
jwang1122 <jwang1122@gmail.com>
2021-05-17 18:43:14 -0500

    pull data from database

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World!</h1>")
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        'title': obj.title,
+        'price': obj.price,
+    }
+    return render(request, "product/detail.html",context)
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view,about_view
+from products.views import product_detail_view
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('home/', home_view, name='home'),
+    path('home/', home_view),
     path('about/', about_view),
+    path('product/', product_detail_view),
     path('admin/', admin.site.urls),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 ]
