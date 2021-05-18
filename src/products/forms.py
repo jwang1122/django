cc7252af89c089478b24ac726a40ae3cc29917c8
jwang1122 <jwang1122@gmail.com>
2021-05-18 16:36:50 -0500

    write your own validation function

--- src/products/forms.py
+++ src/products/forms.py
@@ -2,10 +2,29 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='Enter title',required=True, max_length=80)
+    description = forms.CharField(
+        required=False, 
+        max_length=100, 
+        label='',
+        widget=forms.Textarea(
+            attrs={
+                "placeholder":"Your description",
+                "row":20,
+                "col": 100,
+            }))
+    price = forms.DecimalField(max_digits=10, decimal_places=2)
+
     class Meta:
         model = Product
         fields = ['title', 'description', 'price']
 
+    def clean_title(self,*args,**kwargs):
+        title = self.cleaned_data.get('title')
+        if "CFE" in title:
+            return title
+        else:
+            raise forms.ValidationError("The title must contain 'CFE'.")
 
 class RawProductForm(forms.Form):
     title = forms.CharField(label='Enter title',required=True, max_length=80)
--- src/products/views.py
+++ src/products/views.py
@@ -4,20 +4,20 @@ from .forms import ProductForm, RawProductForm
 
 # Create your views here.
 
-def product_create_view(request):
-    form = RawProductForm()
-    if request.method=='POST':
-        form = RawProductForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            Product.objects.create(**form.cleaned_data)
-            form = RawProductForm()
-        else:
-            print(form.errors)
-    context = {
-        "form":form
-    }
-    return render(request, "products/product_create.html",context)
+# def product_create_view(request):
+#     form = RawProductForm()
+#     if request.method=='POST':
+#         form = RawProductForm(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             Product.objects.create(**form.cleaned_data)
+#             form = RawProductForm()
+#         else:
+#             print(form.errors)
+#     context = {
+#         "form":form
+#     }
+#     return render(request, "products/product_create.html",context)
 
 # Original way to get user input values back
 # def product_create_view(request):
@@ -31,16 +31,16 @@ def product_create_view(request):
 #     return render(request, "products/product_create.html",context)
 
 # using Django Form include values validation
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
 
-#     context = {
-#         'form':form
-#     }
-#     return render(request, "products/product_create.html",context)
+    context = {
+        'form':form
+    }
+    return render(request, "products/product_create.html",context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
