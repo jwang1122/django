00924969d7929e32012ec8863947144b1cdf4ff6
jwang1122 <jwang1122@gmail.com>
2021-05-18 14:49:36 -0500

    Use RawProductForm to grab user's input without saving to database

--- src/products/forms.py
+++ src/products/forms.py
@@ -5,3 +5,9 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['title', 'description', 'price']
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(max_length=80)
+    description = forms.CharField(max_length=100)
+    price = forms.DecimalField(max_digits=10, decimal_places=2)
--- src/products/views.py
+++ src/products/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 # Create your views here.
 
 def product_create_view(request):
-    # print(request.POST)
+    form = RawProductForm()
     if request.method=='POST':
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-        price = request.POST.get('price')
-        Product.objects.create(title=title, description=description, price=price)
-    context = {}
+        form = RawProductForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        else:
+            print(form.errors)
+    context = {
+        "form":form
+    }
     return render(request, "products/product_create.html",context)
 
+# Original way to get user input values back
+# def product_create_view(request):
+#     # print(request.POST)
+#     if request.method=='POST':
+#         title = request.POST.get('title')
+#         description = request.POST.get('description')
+#         price = request.POST.get('price')
+#         Product.objects.create(title=title, description=description, price=price)
+#     context = {}
+#     return render(request, "products/product_create.html",context)
+
+# using Django Form include values validation
 # def product_create_view(request):
 #     form = ProductForm(request.POST or None)
 #     if form.is_valid():
