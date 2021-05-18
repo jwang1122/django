6bd3defe30235a472b0fbc7318f0bb22ce1b31c1
jwang1122 <jwang1122@gmail.com>
2021-05-17 19:00:04 -0500

    send data row directly to view

--- src/products/views.py
+++ src/products/views.py
@@ -5,7 +5,6 @@ from .models import Product
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
     context = {
-        'title': obj.title,
-        'price': obj.price,
+        'object':obj
     }
     return render(request, "product/detail.html",context)
