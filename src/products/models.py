55e58d96e9309e9fe3d7c8d7edef4bf6ad550104
jwang1122 <jwang1122@gmail.com>
2021-05-19 07:37:25 -0500

    get absolute url

--- src/products/models.py
+++ src/products/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def get_absolute_url(self):
+        return f"/products/{self.id}"
