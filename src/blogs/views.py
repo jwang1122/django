aa55d079054d5c4dfc4d759b8889ed99cd65bfc9
jwang1122 <jwang1122@gmail.com>
2021-05-20 00:07:19 -0500

    use BlogListView display list of blogs

--- src/blogs/views.py
+++ src/blogs/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Blog
 from django.views import View
-from django.http import HttpResponse
 
 class BlogListView(View):
+    queryset =  Blog.objects.all()
+
     def get(self, request):
-        # <view logic>
-        return HttpResponse('result')
+        context = {
+            "object_list":self.queryset
+        }
+        return render(request, "blogs/blogs_list_view.html", context)
 
 # Create your views here.
 def blog_list_view(request):
